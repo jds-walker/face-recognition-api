4412f8d51aa9b3dc01a65d1c4dd90a5bebed9915
jds-walker <jds_walker@optusnet.com.au>
2018-12-23 14:56:38 +1100

    requireAuth

--- controllers/signin.js
+++ controllers/signin.js
@@ -72,5 +72,6 @@ const signinAuthentication = (db, bcrypt) => (req, res) => {
 }
 
 module.exports = {
-  signinAuthentication: signinAuthentication
+  signinAuthentication: signinAuthentication,
+  redisClient: redisClient
 }
